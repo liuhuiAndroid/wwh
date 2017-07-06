9e94264665b839d4de5dfca396f88f386b330d35
lh <642404044@qq.com>
2017-07-06 16:57:04 +0800

    自定义View看了两篇不想继续啦

--- view/src/main/java/com/android/wwh/view/MainActivity.java
+++ view/src/main/java/com/android/wwh/view/MainActivity.java
@@ -1,7 +1,9 @@
 package com.android.wwh.view;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void acquaintance(View view){
+        startActivity(new Intent(MainActivity.this,AcquaintanceActivity.class));
+    }
+
+    public void advance(View view){
+        startActivity(new Intent(MainActivity.this,AdvanceActivity.class));
     }
 }
