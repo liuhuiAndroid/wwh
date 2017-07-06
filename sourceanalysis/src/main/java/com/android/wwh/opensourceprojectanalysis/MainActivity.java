8e0bc30774f8e73b0406f9c3e3cc4e1b6deeddcf
lh <642404044@qq.com>
2017-07-06 17:50:22 +0800

    textAllCaps属性修改

--- sourceanalysis/src/main/java/com/android/wwh/opensourceprojectanalysis/MainActivity.java
+++ sourceanalysis/src/main/java/com/android/wwh/opensourceprojectanalysis/MainActivity.java
@@ -1,7 +1,10 @@
 package com.android.wwh.opensourceprojectanalysis;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.MessageQueue;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Handler handler;
+        Looper looper;
+        MessageQueue messageQueue;
+
     }
+
 }
